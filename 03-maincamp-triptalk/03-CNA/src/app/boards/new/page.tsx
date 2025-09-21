502750d38b0ba3881530213aa12b5b480f923af7
jaykxo <jkxojkxo@gmail.com>
2025-09-21 20:32:30 +0900

    fix: update createBoard mutation to return only _id

--- 03-maincamp-triptalk/03-CNA/src/app/boards/new/page.tsx
+++ 03-maincamp-triptalk/03-CNA/src/app/boards/new/page.tsx
@@ -9,8 +9,6 @@ const CREATE_BOARD = gql`
   mutation createBoard($createBoardInput: CreateBoardInput!) {
     createBoard(createBoardInput: $createBoardInput) {
       _id
-      number
-      message
     }
   }
 `;
