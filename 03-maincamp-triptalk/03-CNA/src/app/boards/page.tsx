d4327ea280d7cd55362cb9e6281ac64e56fe77bf
jaykxo <jkxojkxo@gmail.com>
2025-09-22 01:23:29 +0900

    feat: implement deleteBoard mutation with variables and refresh board list via fetchBoards

--- 03-maincamp-triptalk/03-CNA/src/app/boards/page.tsx
+++ 03-maincamp-triptalk/03-CNA/src/app/boards/page.tsx
@@ -1,9 +1,10 @@
 "use client";
 
-import { useQuery, gql } from "@apollo/client";
+import { useQuery, gql, useMutation } from "@apollo/client";
 import Link from "next/link";
 import styles from "./styles.module.css";
 import { formatDate } from "@/utils/formatDate";
+import deleteIcon from "@/assets/icons/delete.png";
 
 const FETCH_BOARDS = gql`
   query fetchBoards($page: Int) {
@@ -16,8 +17,31 @@ const FETCH_BOARDS = gql`
   }
 `;
 
+const DELETE_BOARD = gql`
+  mutation deleteBoard($boardId: ID!) {
+    deleteBoard(boardId: $boardId)
+  }
+`;
+
 export default function BoardListPage() {
   const { data } = useQuery(FETCH_BOARDS, { variables: { page: 1 } });
+  const [deleteBoard, { loading: deleting }] = useMutation(DELETE_BOARD);
+
+  const onClickDelete = async (e: React.MouseEvent, id: string) => {
+    e.stopPropagation();
+    e.preventDefault();
+    alert("삭제되었습니다!");
+    try {
+      await deleteBoard({
+        variables: { boardId: id },
+        refetchQueries: [{ query: FETCH_BOARDS, variables: { page: 1 } }],
+        awaitRefetchQueries: true,
+      });
+    } catch (e: any) {
+      alert(e?.message ?? "삭제 중 오류가 발생했습니다.");
+    }
+  };
+
   return (
     <div className={styles.container}>
       <section className={styles.card}>
@@ -43,6 +67,12 @@ export default function BoardListPage() {
                   {formatDate(board.createdAt)}
                 </span>
               </div>
+              <button
+                className={styles.deleteIcon}
+                onClick={(e) => onClickDelete(e, board._id)}
+              >
+                <img src={deleteIcon.src} alt="삭제" />
+              </button>{" "}
             </li>
           ))}
         </ul>
