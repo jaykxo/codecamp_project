659ea7f20f2392b482b0700eed5cf58a49378e18
jaykxo <jkxojkxo@gmail.com>
2025-10-14 15:03:30 +0900

    feat: commons layout wireframe 컴포넌트 구현
    - Layout 컴포넌트 TSX 파일 생성 (src/commons/layout/index.tsx)
    - CSS 모듈 스타일링 구현 (src/commons/layout/styles.module.css)
    - app/layout.tsx에서 Layout 컴포넌트 연결
    - flexbox 기반 와이어프레임 구조 구현
    - 요구사항 수치값 정확 반영 (header: 60px, banner: 240px, navigation: 48px, footer: 160px)
    - CSS 중복 제거 및 최적화
    - 빌드 성공 확인

--- 04-vibe-coding-emotional-journal/src/app/layout.tsx
+++ 04-vibe-coding-emotional-journal/src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ModalProvider } from "@/commons/provider/modal/modal.provider";
 import { NextThemesProvider } from "@/commons/provider/next-themes/next-themes.provider";
 import { ReactQueryProvider } from "@/commons/provider/react-query/react-query.provider";
+import Layout from "@/commons/layout";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -34,7 +35,9 @@ export default function RootLayout({
         <ReactQueryProvider>
           <NextThemesProvider>
             <ModalProvider>
-              {children}
+              <Layout>
+                {children}
+              </Layout>
             </ModalProvider>
           </NextThemesProvider>
         </ReactQueryProvider>
